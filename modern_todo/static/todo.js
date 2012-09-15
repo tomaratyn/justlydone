271260d0cf244ee31bfb031f9f574155a601e4fc
Tom Aratyn <tom@aratyn.name>
2012-09-15 19:26:26 -0400

    listing the todo lists works now.

--- modern_todo/static/todo.js
+++ modern_todo/static/todo.js
@@ -1,14 +1,31 @@
 
-function () {
-  var ToDoList = Backbone.Model.extend({
-    defaults: {
-      name: ""
-    }
-  })
+ToDoList = Backbone.Model.extend({
+  defaults: {
+    name: ""
+  }
+})
 
-  var ToDoList_List = Backbone.Collection.extend({
-    model: ToDoList
-  })
-} ();
+ToDoList_List = Backbone.Collection.extend({
+  model: ToDoList,
+  url: TODOLISTS_URL,
+  parse: function(response) {
+    return response.objects
+  }
+})
 
+ToDoListView = Backbone.View.extend({
+  el: "ul.todolist_list",
+  collection: new ToDoList_List(),
+  initialize: function() {
+    var self = this
+    this.collection.fetch().success(function(){self.render()})
+  },
+  render: function() {
+    console.log(this.collection)
+    $(this.el).append(this.collection.models.map(function(value, key, list) {
+      return "<li>" + value.attributes.name + "</li>"
+    }))
+  }
+})
 
+todolist_view = new ToDoListView()
