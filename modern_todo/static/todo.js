4bc31893a546c44528f09ec98f62e6a7c2163d37
Tom Aratyn <tom@aratyn.name>
2012-10-04 01:25:49 -0400

    backbone-relational is on the right track.

--- modern_todo/static/todo.js
+++ modern_todo/static/todo.js
@@ -2,33 +2,59 @@
 ToDoList_model = Backbone.RelationalModel.extend({
   defaults: {
     name: ""
-  }
+  },
+  relations: [{
+    type: "HasMany",
+    key: "todos",
+    relatedModel: "ToDo_model",
+    reverseRelation: {
+      key: "list"
+    }
+  }],
+  urlRoot: TODOLISTS_URL
 })
+
 
 ToDo_model = Backbone.RelationalModel.extend({
   defaults: {
     text: ""
-  }
+  },
+  urlRoot: TODO_URL
 })
+
 
 ToDoList_List = Backbone.Collection.extend({
   model: ToDoList_model,
   url: TODOLISTS_URL,
   parse: function(response) {
-    console.log("ToDoList_List::Parse", response)
     return response.objects
   }
 })
 
+ToDoView = Backbone.View.extend(
+
+)
 
 ToDoList_View = Backbone.View.extend({
   events: {
     "click .remove-list": "remove_list",
     "dblclick .name": "rename_list"
   },
-  initialize: function(options){
+  initialize: function(options) {
     this.model.on("destroy", this.remove, this)
     this.model.on("change:name", this.update_name_label, this)
+    this.model.on("add:"+this.model.id, this.onAddTodo, this)
+    this.model.on("reset:"+this.model.id, this.onAddTodo, this)
+    var self = this
+    var mymodel = this.model
+    var todos = this.model.fetchRelated("todos")
+    _.each(todos, function(deferred) {
+      deferred.done(function(fetched_todo) {
+        console.log("have completed todo:", fetched_todo.attributes)
+      })
+    })
+  },
+  onAddTodo:function(addedModel, collection){
   },
   remove_list: function() {
     console.log("ToDoList_View::remove_list")
@@ -51,7 +77,9 @@ ToDoList_View = Backbone.View.extend({
     })
   },
   render: function() {
-    this.setElement(Mustache.render(this.template, this.model.attributes))
+    console.log("ToDoList_View::render", "this.template", this.template, "this.model.attributes", this.model.attributes)
+    var partials = {todo: $("#partial_todo").text()}
+    this.setElement(Mustache.render(this.template, this.model.attributes, partials))
     return this
   },
   template: $("script#list_todolist").text(),
@@ -96,6 +124,7 @@ ToDoList_List_View = Backbone.View.extend({
   render_new_single_list: function(new_todo_list) {
     console.log("ToDoList_List_View::render_new_list", new_todo_list)
     var view = new ToDoList_View({model: new_todo_list})
+    //TODO: don't just use ul here, use ul.classname:
     $(this.el).find("ul").append(view.render().el)
   }
 })
